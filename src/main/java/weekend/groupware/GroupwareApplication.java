8c7ee007af41cbf2a2a5d47414f7af65df1210d3
JeongSu <zz1996zz@naver.com>
2023-10-09 20:08:31 +0900

    make project

---
+++ src/main/java/weekend/groupware/GroupwareApplication.java
@@ -0,0 +1,13 @@
+package weekend.groupware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupwareApplication.class, args);
+	}
+
+}
